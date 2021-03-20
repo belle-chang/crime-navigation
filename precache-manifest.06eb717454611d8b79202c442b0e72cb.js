289614d15176404bbdf199ff34657ac7da6e6358
Anabelle <bellech88@gmail.com>
2021-03-20 17:08:02 -0400

    Updates

---
+++ precache-manifest.06eb717454611d8b79202c442b0e72cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5978ce24769f90dbbd220b1a0afab968",
+    "url": "/crime-navigation/index.html"
+  },
+  {
+    "revision": "3c828a85020f1fcf6af5",
+    "url": "/crime-navigation/static/css/2.76b2b392.chunk.css"
+  },
+  {
+    "revision": "d7ff73657be8ce980d73",
+    "url": "/crime-navigation/static/css/main.7aea9488.chunk.css"
+  },
+  {
+    "revision": "3c828a85020f1fcf6af5",
+    "url": "/crime-navigation/static/js/2.d2eaec46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crime-navigation/static/js/2.d2eaec46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ff73657be8ce980d73",
+    "url": "/crime-navigation/static/js/main.9edc474d.chunk.js"
+  },
+  {
+    "revision": "ee290ac1387d1411f712",
+    "url": "/crime-navigation/static/js/runtime-main.98c637ed.js"
+  }
+]);
