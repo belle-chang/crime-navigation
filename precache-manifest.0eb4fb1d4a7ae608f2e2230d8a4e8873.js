14270839e9c310b023813c24f44f2f55c48c96dd
Anabelle <bellech88@gmail.com>
2021-04-01 00:27:02 -0400

    Updates

---
+++ precache-manifest.0eb4fb1d4a7ae608f2e2230d8a4e8873.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27323cb0d7a38cdcd5c904003a351014",
+    "url": "/crime-navigation/index.html"
+  },
+  {
+    "revision": "8f4705a253a390595d99",
+    "url": "/crime-navigation/static/css/2.76b2b392.chunk.css"
+  },
+  {
+    "revision": "faecdc3f04d9ac876a83",
+    "url": "/crime-navigation/static/css/main.122c9919.chunk.css"
+  },
+  {
+    "revision": "8f4705a253a390595d99",
+    "url": "/crime-navigation/static/js/2.84e6a6f9.chunk.js"
+  },
+  {
+    "revision": "bd2809be49c7652cab6e09fc80ccf40b",
+    "url": "/crime-navigation/static/js/2.84e6a6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faecdc3f04d9ac876a83",
+    "url": "/crime-navigation/static/js/main.5d2b2534.chunk.js"
+  },
+  {
+    "revision": "ee290ac1387d1411f712",
+    "url": "/crime-navigation/static/js/runtime-main.98c637ed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crime-navigation/precache-manifest.a61583f56f2a066fd2a89d68a629ee93.js"
+  "/crime-navigation/precache-manifest.0eb4fb1d4a7ae608f2e2230d8a4e8873.js"
 );
 
 self.addEventListener('message', (event) => {
